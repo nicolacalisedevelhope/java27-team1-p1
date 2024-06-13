95d02e13819c9e80404b5d7b2b90ebdc1d625fb1
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-06-13 15:32:01 +0200

    feat: aggiunta operazione sottrazione

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+
+
+
     }
+    public static float sottrazione(float num1, float num2){
+        return num1 - num2;
+    }
+
 }
