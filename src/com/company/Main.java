faccb5265f5aab1a23fbf640d56a246ce039d385
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-06-13 15:34:55 +0200

    feat: aggiunta operazione moltiplicazione

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public static float moltiplicazione(float num1, float num2){
+        return num1 * num2;
+    }
 }
