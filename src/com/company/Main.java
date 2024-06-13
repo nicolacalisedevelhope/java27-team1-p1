bc65ff31c136cefbfd30b0a1c651f0d797aaa484
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-06-13 16:32:48 +0200

    feat: aggiunto scanner in moltiplicazione

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     }
 
-    public static float moltiplicazione(float num1, float num2){
+    public static float moltiplicazione( Scanner scan){
+        System.out.println("Inserisci il primo numero: ");
+        float num1 = scan.nextFloat();
+        System.out.println("Inserisci il secondo numero: ");
+        float num2 = scan.nextFloat();
         return num1 * num2;
     }
 }
