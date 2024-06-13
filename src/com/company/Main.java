1a678124b2de97569faa1e034fac34a4effb3ebc
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-06-13 15:37:08 +0200

    feat: aggiunta operazione divisione

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public static float divisione(float num1, float num2){
+        if(num2 != 0){
+            return num1 / num2;
+        }else return 0;
+    }
 }
