417bd6234c2d229c08319568db8f26041e0fac3f
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-06-13 16:34:39 +0200

    feat: aggiunto scanner in divisione

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     }
 
-    public static float divisione(float num1, float num2){
+    public static float divisione( Scanner scan ){
+        System.out.println("Inserisci il primo numero: ");
+        float num1 = scan.nextFloat();
+        System.out.println("Inserisci il secondo numero: ");
+        float num2 = scan.nextFloat();
         if(num2 != 0){
             return num1 / num2;
         }else return 0;
