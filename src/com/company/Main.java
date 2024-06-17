b406bdff68057b0983c5d2423c009086f5f57280
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-06-17 17:19:38 +0200

    fix: aggiunto metodo per riconoscere automaticamente operazioni e prendere più numeri in ingresso

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,4 @@
+
 package com.company;
 import java.util.Scanner;
 public class Main {
@@ -6,146 +7,143 @@ public class Main {
         //Dichiaro e istazio lo Scanner
         Scanner sc = new Scanner(System.in);
 
-        //Dichiaro e istazio variabile booleana che permette di fermare o continuare il programma
-        boolean acceso = true;
-
-        // Ciclo While dove utilizzo la variabile accesso come parametro
-        while ( acceso ) {
-
-            //Chiedo all'utente di scegliere il tipo di operazione
-            System.out.println("Seleziona l'operazione da svolgere: ");
-
-            //Salvo l'input dell'utente in una variabile String
-            String operazione = sc.nextLine();
-
-            //Invoco il metodo che permette il riconoscimento dell'operazione inserita dall'utente e l'accesso ai vari metodi
-            riconosciOperazione(operazione, sc);
-
-            //Invoco il metodo per chiedere all'utente se vuole continuare a svolgere operazioni o uscire
-            acceso = continuaUtilizzo(sc, acceso);
-        }
+        //Invoco il metodo  per selezionare l'input
+        sceltaInput(sc);
         sc.close();
     }
 
-    /**
-     * @param operazione Input inserito dall'utente per scegliere l'operazione
-     * @param sc         Scanner per poter accedere ad input dell'utente
-     */
-    public static void riconosciOperazione( String operazione, Scanner sc ) {
-        float risultato;
-        switch ( operazione ) {
+    public static void sceltaInput( Scanner sc){
 
-            case "+":
-                risultato = somma(sc);
-                System.out.println("Il risultato è " + risultato);
-                break;
+        boolean acceso = true;
 
-            case "-":
-                risultato = sottrazione(sc);
-                System.out.println("Il risultato è " + risultato);
-                break;
-
-            case "*":
-                risultato = moltiplicazione(sc);
-                System.out.println("Il risultato è " + risultato);
-                break;
-
-            case "/":
-                risultato = divisione(sc);
-                System.out.println("Il risultato è " + risultato);
-                break;
-
-            case "%":
-                boolean res = controlloPari(sc);
-                if(res){
-                    System.out.println("Il numero è pari");
-                }else System.out.println("Il numero è dispari");
-                break;
-
-            case "^":
-                risultato =potenza(sc);
-                System.out.println("Il risultato è " + risultato);
-                break;
-
-            default:
-                System.out.println("Carattere non riconosciuto!");
-
+        while ( acceso ) { //ciclo while che permette di rieffettuare una scelta dopo il completamento dell'operazione inserita
+            System.out.println("Inserisci 0 per uscire o 1 per inserire un'espressione o 2 per inserire una potenza");
+            int op = sc.nextInt();
+            sc.nextLine();
+            //sceglie l'operazione de eseguire in base al valore immesso
+            switch ( op ) {
+                case 0:
+                    acceso = false;
+                    break;
+                case 2:
+                    //richiama il metodo potenza e ne stampa il risultato
+                    System.out.println("Il risultato della potenza è: " + potenza(sc));
+	                break;
+                case 1:
+                    //richiama il metodo per la scelta dell'operazione
+                    sceltaOperazione(sc);
+	                break;
+            }
         }
+
     }
 
-    /**
-     * @param sc           Scanner per accedere all'input dell'utente
-     * @param interruttore variabile che determina se il programma deve continuare o chiudersi
-     * @return boolean per risposta utente per continuare o chiudere il programma
-     */
+    public static void sceltaOperazione(Scanner sc){
 
-    public static boolean continuaUtilizzo( Scanner sc, boolean interruttore ) {
-        System.out.println("Vuoi eseguire altre operazioni? Premi 0 per uscire");
-        sc.nextLine();
-        String valore = sc.nextLine();
-        if ( valore.equalsIgnoreCase("0") ) {
-            interruttore = false;
-        }
-        return interruttore;
+        System.out.println("Inserisci l'espressione");
+        String espressione = sc.nextLine();
+
+        // Rimuovi spazi bianchi e trimma l'espressione
+        espressione = espressione.replaceAll("\s+", "");
+
+        // Divide l'espressione in base agli operatori +, -,, /, %
+        String[] elementi = espressione.split("(?<=[-+/%*])|(?=[-+/%*])");
+
+        //inizializza la variabile risultato al valore del primo elemento dell'array
+        float risultato = Float.parseFloat(elementi[0]);
+
+        //Invoca la funzione per calcolare l'espressione e ne stampa il risultato
+        System.out.println(calcolaEspressioneMatematica(risultato, elementi));
     }
 
-    public static float sottrazione( Scanner sc ) {
-        System.out.println("Inserisci il primo numero: ");
-        float num1 = sc.nextFloat();
-        System.out.println("Inserisci il secondo numero: ");
-        float num2 = sc.nextFloat();
-        return num1 - num2;
-    }
 
-    public static float potenza( Scanner sc ) {
-        System.out.println("inserisci base");
-        float base = sc.nextFloat();
-        System.out.println("inserisci esponente");
-        int esponente = sc.nextInt();
-        float risultato = base;
-        for ( int i = 1; i < esponente; i++ ) {
-            risultato = risultato * base;
+    public static float calcolaEspressioneMatematica( float risultato, String[] elementi ) {
+
+        //esegue l'operazione corretta per ogni valore inseito
+        for ( int i = 1; i < elementi.length; i += 2 ) {
+            //rileva l'operatore da utilizzare
+            String operatore = elementi[i];
+
+            //rileva il numero da inserire nell'operazione
+            float numero = Float.parseFloat(elementi[i + 1]);
+
+            //sceglie quale operazione effettuare
+            switch ( operatore ) {
+                case "+":
+                    risultato =somma (numero, risultato);
+                    break;
+                case "-":
+                    risultato = sottrazione (numero, risultato);
+                    break;
+                case "*":
+                    risultato = moltiplicazione (numero, risultato);
+                    break;
+                case "/":
+                    risultato =  divisione (numero, risultato);
+                    break;
+                case "%":
+                    controlloPari(numero);
+                    break;
+                default:
+                    System.out.println("Operatore non riconosciuto: " + operatore);
+            }
         }
         return risultato;
     }
 
 
-        public static boolean controlloPari ( Scanner sc){
-            System.out.println("Inserisci il numero da controllare");
-            float num = sc.nextFloat();
-	        return num % 2 == 0;
+        public static float sottrazione ( float numero, float res ){
+            res -= numero;
+            return res;
         }
 
-        public static float somma ( Scanner sc) {
-            System.out.println("Inserisci il primo numero: ");
-            float num1 = sc.nextFloat();
-            System.out.println("Inserisci il secondo numero: ");
-            float num2 = sc.nextFloat();
-            return num1 + num2;
 
+        public static float potenza ( Scanner sc ){
+            System.out.println("inserisci base");
+            float base = sc.nextFloat();
+            System.out.println("inserisci esponente");
+            int esponente = sc.nextInt();
+            float risultato = base;
+            for ( int i = 1; i < esponente; i++ ) {
+                risultato = risultato * base;
+            }
+            return risultato;
         }
 
-        public static float divisione ( Scanner sc){
 
-            System.out.println("Inserisci il primo numero: ");
-            float num1 = sc.nextFloat();
-            System.out.println("Inserisci il secondo numero: ");
-            float num2 = sc.nextFloat();
-            if ( num2 != 0 ) {
-                return num1 / num2;
-            } else return 0;
+        public static void controlloPari ( float numero){
+
+                if ( numero % 2 == 0 ) {
+                    System.out.println("il numero " + numero + " è pari");
+                } else {
+                    System.out.println("il numero " + numero + " è dispari");
+                }
+        }
+
+
+        public static float somma ( float numero, float res){
+            res += numero;
+            return res;
+        }
+
+
+        public static float divisione ( float numeri, float res){
+
+            if ( numeri == 0 ) {
+                System.out.println("Impossibile eseguire divisione per zero");
+                return Float.NaN;
+            } else {
+                res /= numeri;
+            }
+            return res;
+        }
+
+
+        public static float moltiplicazione ( float numero, float res ){
+            res *= numero;
+            return res;
+        }
+
     }
 
-            public static float moltiplicazione ( Scanner sc){
-                System.out.println("Inserisci il primo numero: ");
-                float num1 = sc.nextFloat();
-                System.out.println("Inserisci il secondo numero: ");
-                float num2 = sc.nextFloat();
-                return num1 * num2;
-
-            }
-
-
-
-}
 
